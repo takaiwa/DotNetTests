0fc78fbed41a87ec13d32058a1cb0223e525534e
takaiwa29 <devtakaiwa@gmail.com>
2022-05-26 15:44:13 +0900

    Windowsのログオンユーザー名を取得るテストを追加

---
+++ DotNetTests/Util.cs
@@ -0,0 +1,20 @@
+﻿namespace DotNetTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Util
+    {
+        /// <summary>
+        /// ログオンしているユーザー名を取得.
+        /// </summary>
+        /// <returns>ユーザー名.</returns>
+        public static string GetCurrentName()
+        {
+            return System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+        }
+    }
+}
---
+++ DotNetTestsTests/UtilTests.cs
@@ -0,0 +1,27 @@
+﻿namespace DotNetTests.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using DotNetTests;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class UtilTests
+    {
+        /// <summary>
+        /// ログオンしているユーザー名を取得するテスト.
+        /// </summary>
+        [TestMethod]
+        public void GetCurrentNameTest()
+        {
+            string actual = Util.GetCurrentName();
+
+            Console.WriteLine(string.Format("actual:{0}", actual));
+
+            Assert.AreNotEqual(actual.Length, 0);
+        }
+    }
+}
