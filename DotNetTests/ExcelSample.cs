3b68843f1cbb9f68971fefbc0faf5b67310e975f
takaiwa29 <devtakaiwa@gmail.com>
2022-07-04 14:31:18 +0900

    Excelを扱うサンプルを追加

---
+++ DotNetTests/ExcelSample.cs
@@ -0,0 +1,30 @@
+﻿namespace DotNetTests
+{
+    using System;
+    using System.IO;
+    using NPOI.SS.UserModel;
+    using NPOI.XSSF.UserModel;
+
+    public class ExcelSample
+    {
+        public void CreateNewBook(string path, string sheetName)
+        {
+            try
+            {
+                IWorkbook book = new XSSFWorkbook();
+                book.CreateSheet(sheetName);
+
+                // ブックを保存
+                using (var fs = new FileStream(path, FileMode.Create))
+                {
+                    book.Write(fs);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ DotNetTestsTests/ExcelSampleTests.cs
@@ -0,0 +1,23 @@
+﻿namespace DotNetTests.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using DotNetTests;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class ExcelSampleTests
+    {
+        [TestMethod]
+        public void CreateNewBookTest()
+        {
+            ExcelSample es = new ExcelSample();
+            es.CreateNewBook("foobar.xlsx", "hoge");
+
+
+        }
+    }
+}
