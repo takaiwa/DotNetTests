d4fdaa6a1ef22c99dc51b9f99faf7e7a2c6dcf56
takaiwa29 <devtakaiwa@gmail.com>
2022-10-05 19:13:03 +0900

    JSONを使ったサンプルを追加

---
+++ DotNetTests/SettingSample.cs
@@ -0,0 +1,39 @@
+﻿namespace DotNetTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text.Json;
+    using System.Threading.Tasks;
+
+    // System.Text.Json;はnugetで取得
+    public class SettingSample
+    {
+        public static void JsonSerialize<T>(T data, string jsonFilePath)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            };
+
+            using (var stream = new FileStream(jsonFilePath, FileMode.Create, FileAccess.Write))
+            {
+                JsonSerializer.SerializeAsync(stream, data, options);
+            }
+        }
+
+        public static T JsonDeserialize<T>(string jsonFilePath)
+        {
+            var jsonString = File.ReadAllText(jsonFilePath);
+            return JsonSerializer.Deserialize<T>(jsonString);
+        }
+    }
+
+    public class SettingsData
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ DotNetTestsTests/SettingSampleTests.cs
@@ -0,0 +1,30 @@
+﻿namespace DotNetTests.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using DotNetTests;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class SettingSampleTests
+    {
+        [TestMethod]
+        public void JsonSerializeTest()
+        {
+            string filePath = @"c:\work\settings.json";
+
+            SettingsData sd1 = new SettingsData();
+            sd1.ID = 123;
+            sd1.Name = "foobar";
+
+            SettingSample.JsonSerialize<SettingsData>(sd1, filePath);
+
+            SettingsData sd2 = SettingSample.JsonDeserialize<SettingsData>(filePath);
+
+            Console.WriteLine("ID:" + sd2.ID + ", Name:" + sd2.Name);
+        }
+    }
+}
